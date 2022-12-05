0e02925a1b6513aab4a951d108b21073d2a8fb38
Jason <jhughes3818@gmail.com>
2022-12-06 07:07:00 +1100

    day 2 part 2

--- day-2.js
+++ day-2.js
@@ -14,32 +14,50 @@ const file = fs.readFile("data-day-2.txt", "utf8", (error, data) => {
     console.log(letters);
     letters.forEach((entry) => {
       if (entry[0] === "A" && entry[2] === "X") {
-        //Rock vs Rock Draw = 3 Rock = 1 Total = 4
-        count += 4;
-      } else if (entry[0] === "A" && entry[2] === "Y") {
-        //Rock vs Paper Win = 6 Paper = 2 Total = 8
-        count += 8;
-      } else if (entry[0] === "A" && entry[2] === "Z") {
-        //Rock vs Scissors Lose = 0 Scissors = 3 Total = 3
+        //X = Lose
+        count += 0;
+        //Lost to A = 1
         count += 3;
+      } else if (entry[0] === "A" && entry[2] === "Y") {
+        //Y = Draw
+        count += 3;
+        //Draw with A = 1
+        count += 1;
+      } else if (entry[0] === "A" && entry[2] === "Z") {
+        //Z = Win
+        count += 6;
+        //Win Against A = 2
+        count += 2;
       } else if (entry[0] === "B" && entry[2] === "X") {
-        //Paper vs Rock Lose = 0 Rock = 1 Total = 1
+        // X = Lose
+        count += 0;
+        //B=3
         count += 1;
       } else if (entry[0] === "B" && entry[2] === "Y") {
-        //Paper vs Paper Draw = 3 Paper = 2 Total = 5
-        count += 5;
-      } else if (entry[0] === "B" && entry[2] === "Z") {
-        //Paper vs Scissors Win = 6 Scissors = 3 Total = 9
-        count += 9;
-      } else if (entry[0] === "C" && entry[2] === "X") {
-        //Scissors vs Rock Win = 6 Rock = 1 Total = 7
-        count += 7;
-      } else if (entry[0] === "C" && entry[2] === "Y") {
-        //Scissors vs Paper Lose = 0 Paper = 2 Total = 2
+        //Y = Draw
+        count += 3;
+        //B=3
         count += 2;
-      } else if (entry[0] === "C" && entry[2] === "Z") {
-        //Scissors vs Scissors Draw = 3 Scissors = 3 Total = 6
+      } else if (entry[0] === "B" && entry[2] === "Z") {
+        //Z = Win
         count += 6;
+        //B=3
+        count += 3;
+      } else if (entry[0] === "C" && entry[2] === "X") {
+        // X= Lose
+        count += 0;
+        //C=1
+        count += 2;
+      } else if (entry[0] === "C" && entry[2] === "Y") {
+        //Y = Draw
+        count += 3;
+        //C=1
+        count += 3;
+      } else if (entry[0] === "C" && entry[2] === "Z") {
+        //Z = Win
+        count += 6;
+        //C=1
+        count += 1;
       }
     });
 
